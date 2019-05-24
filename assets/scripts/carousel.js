ce4578adbd32382903e04400027f98e1b14c0ff6
spencer <spencer.mcw@gmail.com>
2019-05-24 10:54:52 -0700

    Adds Carousels for project images

---
+++ assets/scripts/carousel.js
@@ -0,0 +1,14 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    // nav:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        900:{
+            items:4
+        }
+    }
+})
